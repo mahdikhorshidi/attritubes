e62b3a92a886463f76dfd01f46af724bd5a18019
Mahdi Khorshidi <mahdikhorshidi@gmail.com>
2017-12-24 15:55:35 +0330

    first commit

---
+++ src/attributesServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace mahdikhorshidi\attributes;
+
+use Illuminate\Support\ServiceProvider;
+
+class attributesServiceProvider extends ServiceProvider
+{
+    /**
+     * Perform post-registration booting of services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register any package services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
